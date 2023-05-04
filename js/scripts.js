e6bb57279cbeccf501773c1b41a56a4e5f037684
Alan <115197222+DevABJ@users.noreply.github.com>
2023-05-04 17:10:05 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,39 @@ const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
+const menuMobile = document.querySelector("mobile-navbar")
+
+
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
 const slides = document.querySelectorAll(".banner");
 const dots = document.querySelectorAll(".dot");
 let slideIndex = 0;
 
 // Funções
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior:  "smooth",
+  });
+
+  setTimeout(() => {
+
+    if (menu.classList.contains("menu-active")) {
+      menu.classList.remove("menu-active");
+    }
+
+  }, 500)
+
+
+}
 
 function showSlides() {
   for (let i = 0; i < slides.length; i++) {
@@ -32,6 +60,13 @@ function showSlides() {
   btn.addEventListener("click", (e) => {
     menu.classList.toggle("menu-active");
   });
+});
+
+
+/* evento SMOOTH SROLL*/
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", smoothScroll);
 });
 
 
